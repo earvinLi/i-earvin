5df9e24b2802b1ce0d5a97ee3ba65085041f2af0
Earvin Li <earvin.tli@gmail.com>
2019-02-23 17:29:44 +0800

    [App] Change to use .jsx extension

---
+++ src/App/index.jsx
@@ -0,0 +1,18 @@
+// External Dependencies
+import React from 'react';
+
+// Local Dependencies
+import Coding from '../Coding';
+import TopNavigation from './TopNavigation';
+
+// Component Definition
+function App() {
+  return (
+    <div>
+      <TopNavigation />
+      <Coding />
+    </div>
+  );
+}
+
+export default App;
