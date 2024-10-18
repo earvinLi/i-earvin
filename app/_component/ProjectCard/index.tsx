2109df2fa68506260e49ea72c8d4bfab65d77cda
Earvin Li <tianyang.li@bytedance.com>
2024-10-18 12:30:11 +0800

    ⭐ feat: init proj with 'TypeScript'

---
+++ app/_component/ProjectCard/index.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+
+import projectCardStyles from './ProjectCard.module.css';
+
+const {
+  Page_projectCardContainerStyle,
+  Page_projectCardTextSectionStyle,
+  Page_projectCardTitleStyle,
+  Page_projectCardDescriptionStyle,
+  Page_projectCardTechListStyle,
+  Page_projectCardTechListItemStyle,
+  Page_projectCardTechLinkStyle,
+} = projectCardStyles;
+
+type Tech = {
+  techName: string;
+  techDescription: string;
+  techLink: string;
+};
+
+type ProjectCardProps = {
+  title: string;
+  description: string;
+  techList: Tech[];
+}
+
+function ProjectCard(props: ProjectCardProps) {
+  const {
+    title,
+    description,
+    techList,
+  } = props;
+
+  return (
+    <div className={Page_projectCardContainerStyle}>
+      <Image src='/project_cover_whyb.jpg' width={320} height={240}/>
+      <div className={Page_projectCardTextSectionStyle}>
+        <div className={Page_projectCardTitleStyle}>{title}</div>
+        <div className={Page_projectCardDescriptionStyle}>{description}</div>
+        <div className={Page_projectCardTechListStyle}>
+          {techList.map((tech: Tech) => {
+            const {
+              techName,
+              techDescription,
+              techLink,
+            } = tech;
+
+            return (
+              <li key={techName} className={Page_projectCardTechListItemStyle}>
+                <a href={techLink} target='_blank' className={Page_projectCardTechLinkStyle}>{techName}</a> - {techDescription}
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
