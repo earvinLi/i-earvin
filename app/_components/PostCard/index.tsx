7f8c633815153871d40bb10aace8d356429c1842
Earvin Li <tianyang.li@bytedance.com>
2024-11-05 12:30:17 +0800

    feat: optimize 'AppNavigation' styles

--- app/_components/PostCard/index.tsx
+++ app/_components/PostCard/index.tsx
@@ -24,40 +24,43 @@ function PostCard(props: PostCardProps) {
   } = props;
 
   return (
-    <div className="flex items-center mb-8">
-      <div className="w-80 h-60">
-        <OptimizedImage
-          alt={`Cover image for ${title}`}
-          src={coverImage.url}
-          width={320}
-          height={240}
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <div className="flex flex-col grow ml-16">
-        <Link
-          href={`/posts/${slug}`}
-          className="hover:underline text-3xl leading-snug mb-6"
-        >
-          {title}
-        </Link>
-        <div className="text-lg leading-relaxed mb-6">{excerpt}</div>
-        <div className="flex">
-          <div className="mr-8">
-            {author && (
-              <Avatar
-                name={author.name}
-                image={author.picture.url}
-                direction='horizontal'
-                size="small"
-              />
-            )}
+    <div className="mb-6">
+      <div className="flex items-center">
+        <div className="w-60 h-40">
+          <OptimizedImage
+            alt={`Cover image for ${title}`}
+            src={coverImage.url}
+            width={240}
+            height={160}
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="flex flex-col grow ml-12">
+          <Link
+            href={`/posts/${slug}`}
+            className="hover:underline text-3xl leading-snug mb-4"
+          >
+            {title}
+          </Link>
+          <div className="text-lg leading-relaxed mb-4">{excerpt}</div>
+          <div className="flex">
+            <div className="mr-6">
+              {author && (
+                <Avatar
+                  name={author.name}
+                  image={author.picture.url}
+                  direction='horizontal'
+                  size="small"
+                />
+              )}
+            </div>
+            <time dateTime={date} className="text-sm">
+              {format(new Date(date), "LLLL	d, yyyy")}
+            </time>
           </div>
-          <time dateTime={date} className="text-sm">
-            {format(new Date(date), "LLLL	d, yyyy")}
-          </time>
         </div>
       </div>
+      <hr className="border-accent-2 mt-6"/>
     </div>
   );
 }
--- app/_modules/AppNavigation/index.js
+++ app/_modules/AppNavigation/index.js
@@ -1,10 +1,44 @@
+'use client';
+
+import classNames from 'classnames';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const pageData = [
+  { name: 'Projects', path: '/' },
+  { name: 'Posts', path: '/posts' },
+];
+
+const appNavigationStyles = {
+  font: {
+    current: 'text-lg text-[#7d97ad]',
+    other: 'text-lg',
+  },
+};
+
+const checkIsCurrentPath = (currentPath, route) => currentPath === route;
 
 function AppNavigation() {
+  const pathname = usePathname();
+
   return (
-    <div className="w-3/4 mx-auto">
-      <Link href="/">Projects</Link>
-      <Link href="/posts">Posts</Link>
+    <div className="w-[70%] mx-auto my-4">
+      {pageData.map((page) => {
+        const { name, path } = page;
+        const isCurrentPage = checkIsCurrentPath(pathname, path);
+        const fontStyle = isCurrentPage ? appNavigationStyles.font.current : appNavigationStyles.font.other;
+
+        return (
+          <Link
+            key={`${path}-${name}`}
+            href={path}
+            className={classNames('mr-6 hover:underline', fontStyle)}
+          >
+            {name}
+          </Link>
+        );
+      })}
     </div>
   );
 }
