82e6f2dc5126d56b6f63adb91dc9abde24db7134
Earvin Li <earvin.tli@gmail.com>
2024-12-16 12:02:32 +0800

    fix: correct home page redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/projects',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
