0952e2d859cb42df143f56a6e7a039462ff88784
Earvin Li <earvin.tli@gmail.com>
2025-01-15 12:55:56 +0800

    feat: add label to 'TiptapEditor'

--- app/_components/TiptapEditor/index.tsx
+++ app/_components/TiptapEditor/index.tsx
@@ -6,17 +6,19 @@ import StarterKit from '@tiptap/starter-kit';
 import TiptapEditorToolbar from './TiptapEditorToolbar';
 
 type TiptapEditorProps = {
-  onChange: (value: string) => void;
+  label: string;
   value: string;
+  onChange: (value: string) => void;
 }
 
 // Component Definition
 export default function TiptapEditor(props: TiptapEditorProps) {
-  const { onChange, value } = props;
+  const { label, value, onChange } = props;
 
   const editor = useEditor({
     extensions: [StarterKit],
     content: value,
+    // Todo: build proper focus and hover styles
     editorProps: { attributes: { class: 'h-[180px] overflow-y-scroll focus:outline-none' } },
     onUpdate({ editor: currentEditor }) {
       onChange(currentEditor.getHTML());
@@ -24,9 +26,12 @@ export default function TiptapEditor(props: TiptapEditorProps) {
   });
 
   return (
-    <div className="w-full border-2 border-gray-300 rounded p-3 flex flex-col gap-2">
-      <TiptapEditorToolbar editor={editor} />
-      <EditorContent editor={editor} />
+    <div className="flex flex-col gap-2">
+      <div className="text-gray-400 text-sm">{label}</div>
+      <div className="w-full border-2 border-gray-300 rounded p-3 flex flex-col gap-2">
+        <TiptapEditorToolbar editor={editor} />
+        <EditorContent editor={editor} />
+      </div>
     </div>
   );
 }
